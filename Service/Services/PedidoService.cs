88880077cb1de0973ee9d2ced7c546f0d065f6e6
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-02-20 00:35:01 -0300

    add pedido service

---
+++ Service/Services/PedidoService.cs
@@ -0,0 +1,13 @@
+﻿using Service.Interfaces;
+using System.Threading.Tasks;
+
+namespace Service.Services
+{
+    public class PedidoService : IPedidoService
+    {
+        public Task<int> GetId()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
