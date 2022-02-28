f71e5d895aea63fad5eb5ee1e0f5e9e22b15dcb9
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-02-28 20:09:26 -0300

    add producto managment

---
+++ Service/Services/ProductoService.cs
@@ -0,0 +1,12 @@
+﻿using Interface.Interfaces;
+using Persistence.Data;
+using Persistence.Models;
+using Repository;
+
+namespace Service.Services
+{
+    public class ProductoService: EntityBaseRepository<Producto> ,IProductoService
+    {
+        public ProductoService(EcommerceContext context) : base(context) { }
+    }
+}
