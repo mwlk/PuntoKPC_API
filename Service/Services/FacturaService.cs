c423151fbc3462ddc00b74f8163fa6600c8584ee
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-02-28 21:09:33 -0300

    add services

---
+++ Service/Services/FacturaService.cs
@@ -0,0 +1,14 @@
+﻿using Interface.Interfaces;
+using Persistence.Data;
+using Persistence.Models;
+using Repository;
+
+namespace Service.Services
+{
+    public class FacturaService : EntityBaseRepository<Factura>, IFacturaService
+    {
+        public FacturaService(EcommerceContext context) : base(context)
+        {
+        }
+    }
+}
