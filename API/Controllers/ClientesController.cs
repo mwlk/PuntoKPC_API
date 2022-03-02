6f65841203b0d94ee4166a2b1ff6162761054e3d
Mirko Wlk <54175564+mwlk@users.noreply.github.com>
2022-03-01 23:05:40 -0300

    add clients controller

---
+++ API/Controllers/ClientesController.cs
@@ -0,0 +1,48 @@
+﻿using Interface.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Persistence.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private readonly IClienteService _service;
+
+        public ClientesController(IClienteService service) => _service = service;
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _service.GetAllAsync();
+
+            if (result == null) return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Cliente model)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            try
+            {
+                await _service.AddAsync(model);
+
+                return Ok(model);
+            }
+            catch (Exception e)
+            {
+                return Problem("client post fail", e.Message);
+            }
+        }
+
+    }
+}
